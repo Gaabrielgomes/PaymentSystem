13b884dc70de77c6451fb8214ddcc41adfb80ed7
Gaabrielgomes <gaabrielgomes3241@gmail.com>
2026-09-17 16:03:45 -0300

    feat: adds RabbitMQ credentials configuration

--- app/core/config.py
+++ app/core/config.py
@@ -2,6 +2,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     database_url: str
+    rabbitmq_user: str
+    rabbitmq_pass: str
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
